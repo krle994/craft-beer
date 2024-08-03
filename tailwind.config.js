14f53cc108fcf130b9b256ad04f981f457388b6f
Milan Krstic <krlekrstic018@gmail.com>
2024-08-03 12:27:28 +0200

    Setup config and packages

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        "auto-3": "repeat(auto-fit, minmax(320px, 1fr))",
+      },
+    },
+  },
+  plugins: [],
+};
